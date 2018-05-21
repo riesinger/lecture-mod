01fb936c6d83ead75460145b04e3b47c0e84e65d
riesinger <riesinger.pascal@gmail.com>
2018-05-21 23:51:27 +0200

    Work on UI for custom selectors

--- router.go
+++ router.go
@@ -7,19 +7,12 @@ import (
 
 func startRouter() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/rapla/ai", func(w http.ResponseWriter, r *http.Request) {
-		mtx.RLock()
-		w.Write([]byte(aiLectures))
-		mtx.RUnlock()
-	})
-	mux.HandleFunc("/rapla/mi", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/rapla/get/", func(w http.ResponseWriter, r *http.Request) {
 		mtx.RLock()
 		w.Write([]byte(miLectures))
 		mtx.RUnlock()
 	})
-	mux.HandleFunc("/rapla", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("See <a href='/rapla/ai'>AI calendar</a> or <a href='/rapla/mi'>MI calendar</a>"))
-	})
+	mux.Handle("/rapla/", http.StripPrefix("/rapla/", http.FileServer(http.Dir("client"))))
 
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:10944", mux))
