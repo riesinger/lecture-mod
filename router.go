1f95a700aaed56bfe1f35a93e2c8d118b756cf33
riesinger <riesinger.pascal@gmail.com>
2018-05-22 17:58:40 +0200

    Add handler for all options checked

--- router.go
+++ router.go
@@ -19,6 +19,17 @@ func startRouter() {
 		}
 		w.Write(output)
 	})
+	m.HandleFunc("/rapla/get/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("GET", r.URL)
+		m := make(map[string]string)
+		output, err := generateCalendar(m)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Fatal(err)
+			return
+		}
+		w.Write(output)
+	})
 	m.Handle("/rapla/", http.StripPrefix("/rapla/", http.FileServer(http.Dir("client"))))
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:10944", m))
